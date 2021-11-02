4a5ec4a2c4c004dc2027d11bf93fe74405c12176
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-02 15:35:29 +0100

    productsValidation added

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { validationResult } from "express-validator";
+import { productsValidation } from "./validation.js";
 import createHttpError from "http-errors";
 import multer from "multer";
 
