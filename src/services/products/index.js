31681a518c9f2250813bd080327cd988f3433546
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-02 17:01:55 +0100

    get single product

--- src/services/products/index.js
+++ src/services/products/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import db from "../../db/connect.js";
 import { validationResult } from "express-validator";
 import { productsValidation } from "./validation.js";
 import createHttpError from "http-errors";
@@ -6,25 +7,7 @@ import multer from "multer";
 
 const productsRouter = express.Router();
 
-//************************ GET ALL THE PRODUCTS ********************************
-productsRouter.get("/", async (req, res, next) => {
-  try {
-    if (req.query && req.query.category) {
-      const { category } = req.query;
-
-      const filteredProducts = await db.query(
-        `SELECT * FROM products WHERE category LIKE '%${category}%'`
-      );
-
-      res.send(filteredProducts.rows);
-    }
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-});
-
-//************************ GET ALL THE PRODUCTS ********************************
+//************************  POST A PRODUCT ********************************
 productsRouter.post("/", productsValidation, async (req, res, next) => {
   try {
     const errorList = validationResult(req);
@@ -37,6 +20,48 @@ productsRouter.post("/", productsValidation, async (req, res, next) => {
       );
 
       res.status(201).send(newProduct.rows[0]);
+    } else {
+      next(createHttpError(400, { errorList }));
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+//************************  GET SINGLE PRODUCT ********************************
+productsRouter.get("/:id", async (req, res, next) => {
+  try {
+    const paramsID = req.params.id;
+    const product = await db.query(
+      `SELECT * FROM products WHERE id=${paramsID}`
+    );
+    if (product.rows.length > 0) {
+      res.send(product.rows[0]);
+    } else {
+      res.send(
+        createHttpError(
+          404,
+          `The Product with the id: ${paramsID} was not found.`
+        )
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+//************************ GET ALL THE PRODUCTS ********************************
+productsRouter.get("/", async (req, res, next) => {
+  try {
+    if (req.query && req.query.category) {
+      const { category } = req.query;
+
+      const filteredProducts = await db.query(
+        `SELECT * FROM products WHERE category LIKE '%${category}%'`
+      );
+
+      res.send(filteredProducts.rows);
     } else {
       next(createHttpError(400, { errorList }));
     }
